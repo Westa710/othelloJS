3a880973b29f5a449c6fb4209d8fd7c182cdbdaa
ko <pleiades1424@gmail.com>
2024-01-27 12:39:13 +0900

    commit

--- script.js
+++ script.js
@@ -32,46 +32,87 @@ function setStone(row, column, unconditionallySet) {
 }
 
 function canSetStone(row, column) {
+  //searchReversibleSquareから返された二次元配列の中に、othelloData[row][column]が
+  //含まれていたらtrueを返す
+
   if (othelloData[row][column] != 0) {
     return false;
-  } else {
-    const direction = [
-      [-1, 0], // 左
-      [-1, 1], // 左下
-      [0, 1], // 下
-      [1, 1], // 右下
-      [1, 0], // 右
-      [1, -1], // 右上
-      [0, -1], // 上
-      [-1, -1], // 左上
-    ];
+  }
 
-    let reversibleSquare = [];
+  const reversibleArr = searchTurningStone(row, column);
 
-    for (let i = 0; i < direction.length; i++) {
-      reversibleSquare.push(searchReversibleSquare(row, column, direction[i][0], direction[i][1]));
+  console.log(reversibleArr);
+
+  // reversibleArr.includes();
+
+  // 返された配列を検索し、その中に[row][column]が含まれているか調べる
+
+  return true;
+}
+
+function searchTurningStone(sRow, sColumn) {
+  //この関数に引数として(座標)を与えることでsearchを集約する
+  //返り値：[sRow][sColumn]に置いたとき、ひっくり返る石の座標(二次元配列)
+
+  const direction = [
+    [-1, 0], // 左
+    [-1, 1], // 左下
+    [0, 1], // 下
+    [1, 1], // 右下
+    [1, 0], // 右
+    [1, -1], // 右上
+    [0, -1], // 上
+    [-1, -1], // 左上
+  ];
+
+  let reversibleSquare = [];
+
+  //置く石の色が黒の場合のみ実装してみる
+
+  for (let i = 0; i <= 7; i++) {
+    let dx = sRow + direction[i][0];
+    let dy = sColumn + direction[i][1];
+
+    if (dx >= 0 && dy >= 0 && dx <= 7 && dy <= 7 && othelloData[dx][dy] === (othelloColor ? -1 : 1)) {
+      let tmpReverseStone = [];
+      let reverseStone = [];
+      tmpReverseStone.push([dx, dy]);
+      // console.log(`[${dx}][${dy}]をpush`);
+
+      while (true) {
+        dx += direction[i][0];
+        dy += direction[i][1];
+
+        if (dx < 0 || dy < 0 || dx > 7 || dy > 7 || othelloData[dy][dx] === 0) {
+          tmpReverseStone = [];
+          break;
+        }
+
+        if (othelloData[dx][dy] === (othelloColor ? -1 : 1)) {
+          tmpReverseStone.push([dx, dy]);
+        }
+
+        if (othelloData[dx][dy] === (othelloColor ? 1 : -1)) {
+          reverseStone = reverseStone.concat(tmpReverseStone);
+          tmpReverseStone = [];
+          break;
+        }
+      }
+      reversibleSquare = reversibleSquare.concat(reverseStone);
     }
   }
 
-  console.log(reversibleSquare);
-
-  return;
+  return reversibleSquare;
 }
 
-function searchReversibleSquare(row, column, dx, dy) {
-  //この関数に引数として(座標)を与えることでsearchを集約する
-  let x = row + dx;
-  let y = column + dy;
+// function searchOneDirection(squareArr, row, column, dx, dy) {
+//   if (othelloData[row + dx][column + dy] === othelloColor ? -1 : 1) {
+//     searchOneDirection(squareArr, row + dx, column + dy, dx, dy);
+//     squareArr.push([row + dx, column + dy]);
+//   }
 
-  let tmpReversibleSquare = [];
-
-  if (othelloData[x][y] === othelloColor ? -1 : 1) {
-    tmpReversibleSquare.push(searchReversibleSquare(x, y, dx, dy));
-    tmpReversibleSquare.push([x, y]);
-  }
-
-  return tmpReversibleSquare;
-}
+//   return squareArr;
+// }
 
 window.onload = function () {
   //盤面の作成
@@ -105,4 +146,6 @@ window.onload = function () {
   setStone(3, 3, true);
   setStone(4, 3, true);
   setStone(4, 4, true);
+  setStone(5, 3, true);
+  setStone(5, 4, true);
 };
